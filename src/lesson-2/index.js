f8bd002e16df9b7a8a72456ae2c548210e2fb2b4
wang xiu <xiu.wang@thoughtworks.com>
2020-01-06 15:50:23 +0800

    Add test case for taking package and implement taking package

--- src/lesson-2/index.js
+++ src/lesson-2/index.js
@@ -1,10 +1,31 @@
+import { NO_AVAILABLE_BOX } from './constant'
+
 export class Locker {
-    constructor() {
-        this.box = 24
+    constructor(size) {
+        this.boxSize = size
+        this.max = size
     }
 
-    printBarcode() {
-        this.box -= 1
-        return this.box
+    savePackage() {
+        let result = NO_AVAILABLE_BOX
+        if (this.boxSize) {
+            // eslint-disable-next-line prefer-destructuring
+            result = this.boxSize
+        }
+        this.boxSize -= 1
+        return result
+    }
+
+    takePackage(barcode) {
+        let result
+        if (
+            typeof barcode === 'number' &&
+            barcode > this.boxSize &&
+            barcode <= this.max
+        ) {
+            this.boxSize += 1
+            result = barcode
+        }
+        return result
     }
 }
